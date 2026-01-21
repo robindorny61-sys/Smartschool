21ef6aaf1eea63082945628036f0703df409b448
robindorny61-sys <robindorny61@gmail.com>
2026-01-21 20:08:48 +0100

    Create vak.js

---
+++ css/js/js/js/vak.js
@@ -0,0 +1,5 @@
+// Vakpagina (dummy content)
+document.addEventListener('DOMContentLoaded', () => {
+    const vakNaam = document.getElementById('vakNaam');
+    vakNaam.textContent = 'Wiskunde 3A'; // later dynamisch
+});
