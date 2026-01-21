45aca3221f06db5d7ec789bbceaa54e9bdb73f96
robindorny61-sys <robindorny61@gmail.com>
2026-01-21 20:24:18 +0100

    Update main.js

--- css/js/main.js
+++ css/js/main.js
@@ -1,15 +1,26 @@
-// Globale functies
+// Test login
 document.addEventListener('DOMContentLoaded', () => {
     const loginForm = document.getElementById('loginForm');
-    if(loginForm){
+
+    // Dit is ons testaccount
+    const testUser = {
+        username: 'admin',
+        password: '1234'
+    };
+
+    if (loginForm) {
         loginForm.addEventListener('submit', e => {
             e.preventDefault();
             const username = document.getElementById('username').value;
             const password = document.getElementById('password').value;
-            alert(`Inloggen: ${username}`);
-            /const testUser = {
-        username: 'admin',
-        password: '1234'/ 
+
+            if (username === testUser.username && password === testUser.password) {
+                alert('Login succesvol!');
+                // Ga door naar dashboard
+                window.location.href = 'dashboard-leraar.html';
+            } else {
+                alert('Foutieve gebruikersnaam of wachtwoord');
+            }
         });
     }
 });
